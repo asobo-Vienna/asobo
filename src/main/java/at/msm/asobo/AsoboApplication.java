844fcff569abee3d50574e2daf2db1eb81c47bf5
martin wallner <ww24e022@technikum-wien.at>
2025-05-12 13:38:34 +0200

    email

---
+++ src/main/java/at/msm/asobo/AsoboApplication.java
@@ -0,0 +1,13 @@
+package at.msm.asobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsoboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsoboApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/msm/asobo/AsoboApplicationTests.java
@@ -0,0 +1,13 @@
+package at.msm.asobo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsoboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
