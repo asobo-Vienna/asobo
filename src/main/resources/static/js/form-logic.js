b0fcd16fd5998aebf266137158b7a6a7153ef0b1
seimes <simon.wolfsteiner@a1.net>
2025-05-22 09:06:51 +0200

    Updated the register form

--- src/main/resources/static/js/form-logic.js
+++ src/main/resources/static/js/form-logic.js
@@ -1,3 +1,4 @@
+// event listener for salutation selection
 document.addEventListener('DOMContentLoaded', () => {
     const toggleSalutationSelect = document.getElementById('salutation');
     const otherInput = document.getElementById('salutation-other-group');
@@ -16,4 +17,22 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // And again when changed
     toggleSalutationSelect.addEventListener('change', updateVisibility);
+
+    emailValidation();
 });
+
+function emailValidation() {
+    const emailInput = document.getElementById('register-email');
+    const emailError = document.getElementById('register-email-error');
+
+    emailInput.addEventListener('input', () => {
+        const value = emailInput.value;
+        const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+
+        if (value && !isValid) {
+            emailError.style.display = 'block';
+        } else {
+            emailError.style.display = 'none';
+        }
+    });
+}
