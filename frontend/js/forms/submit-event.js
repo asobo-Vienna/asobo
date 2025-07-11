8ea5c7ff9da9adb8d37ca3354fcda4b58e818a12
seimes <simon.wolfsteiner@a1.net>
2025-07-11 10:55:47 +0200

    Replaced create event jquery request by javascript fetch

--- frontend/js/forms/submit-event.js
+++ frontend/js/forms/submit-event.js
@@ -2,18 +2,34 @@ $("#submit-event-button").on("click", async function (e) {
     e.preventDefault();
 
     let formData = new FormData();
+
+    const datePart = $("input[name='datePart']").val();
+    const timePart = $("input[name='timePart']").val();
+
+    if (!datePart || !timePart) {
+        alert("Please select both date and time.");
+        return;
+    }
+
+    const combinedDateTime = `${datePart}T${timePart}`;
+
+    formData.append("date", combinedDateTime);
     formData.append("category", $("input[name='category']").val());
     formData.append("title", $("input[name='title']").val());
-    formData.append("date", $("input[name='date']").val());
+    formData.append("date", combinedDateTime);
     formData.append("description", $("input[name='description']").val());
     formData.append("location", $("input[name='location']").val());
 
-    const fileInput = $("input[name='event_picture']")[0];
+    const fileInput = $("input[name='eventPicture']")[0];
     if (fileInput && fileInput.files.length > 0) {
         formData.append("eventPicture", fileInput.files[0]);
     }
 
     const url = HOSTADDRESS + '/api/events';
+
+    for (let [key, value] of formData.entries()) {
+        console.log(key, value);
+    }
 
     try {
         const response = await fetch(url, {
@@ -22,18 +38,20 @@ $("#submit-event-button").on("click", async function (e) {
         });
 
         if (!response.ok) {
-            throw new Error(`Response status: ${response.statusText}`);
+            console.error(`Request failed. Status: ${response.status} (${response.statusText})`);
+            throw new Error(`Request failed: ${response.status}`);
         }
 
         const data = await response.json();
-        $(".register-form").html(`Event: ${data.title} created successfully`);
+        //$(".register-form").html(`Event: ${data.title} created successfully`);
         console.log("Created event:", data);
     } catch (error) {
-        console.error('Error while creating event: ' + error.message);
+        //console.error('Error while creating event: ' + error.message);
+        console.error('Network or fetch error:', error);
     }
 
     /*$.ajax({
-        url: "/api/events",
+        url: HOSTADDRESS + "/api/events",
         type: "POST",
         data: formData,
         processData: false,
