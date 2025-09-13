bf7dfc86e0cbc77c1031e637313067e1de43475c
seimes <simon.wolfsteiner@a1.net>
2025-09-13 21:35:53 +0200

    Add missing imports in SecurityConfig.java

--- backend/src/main/java/at/msm/asobo/config/SecurityConfig.java
+++ backend/src/main/java/at/msm/asobo/config/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import at.msm.asobo.security.TokenAuthenticationFilter;
+import at.msm.asobo.security.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
