15f55dc7ffe81585c1c89b9d28905826436ff94e
Mike OneFive <notsure@riseup.net>
2025-11-19 20:14:07 +0100

    #63 feat: basic list of users for admin page

--- frontend-angular/src/app/features/admin/admin-user-list/admin-user-list.ts
+++ frontend-angular/src/app/features/admin/admin-user-list/admin-user-list.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { TagModule } from 'primeng/tag';
 import { User } from '../../auth/models/user';
 import { DatePipe } from '@angular/common';
+import {UserService} from '../services/user-service';
+import {UrlUtilService} from '../../../shared/utils/url/url-util-service';
 
 @Component({
   selector: 'app-admin-user-list',
@@ -15,5 +17,28 @@ import { DatePipe } from '@angular/common';
   styleUrl: './admin-user-list.scss',
 })
 export class AdminUserList {
+  private userService = inject(UserService);
   users: User[] = [];
+
+  ngOnInit(): void {
+      this.userService.getAllUsers().subscribe({
+        next: (users) => {
+          this.users = users;
+          console.log(this.users);
+        },
+        error: (err) => console.error('Error fetching users:', err)
+      });
+      return;
+  }
+
+  protected readonly UrlUtilService = UrlUtilService;
+
+  onEdit(user: any) {
+    console.log('Editing user:', user);
+  }
+
+  onDelete(user: any) {
+    console.log('Deleting user:', user);
+  }
 }
+
