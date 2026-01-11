65635f4598a27492de1be430d051ec70715f7695
Mike OneFive <notsure@riseup.net>
2026-01-11 21:23:54 +0100

    fix: exception http status code

--- backend/src/main/java/at/msm/asobo/exceptions/EventAdminException.java
+++ backend/src/main/java/at/msm/asobo/exceptions/EventAdminException.java
@@ -1,5 +1,9 @@
 package at.msm.asobo.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class EventAdminException extends RuntimeException {
     public EventAdminException(String message) {
         super(message);
