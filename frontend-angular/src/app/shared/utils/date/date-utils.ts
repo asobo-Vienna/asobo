ede8f2a9df667be21a073a7e8bfbcb3cef195e63
Mike OneFive <notsure@riseup.net>
2026-01-17 16:44:43 +0100

    feat: date-utils.ts for frontend UTC time and local time discrepancy

---
+++ frontend-angular/src/app/shared/utils/date/date-utils.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DateUtils {
+  public static toLocalISOString(date: Date): string {
+    const offset = date.getTimezoneOffset();
+    const localDate = new Date(date.getTime() - offset * 60 * 1000);
+    return localDate.toISOString().slice(0, -1);
+  }
+}
