550039420d50b2939a7a2ccd68f8c4d9a83e2dbf
seimes <simon.wolfsteiner@a1.net>
2026-02-10 16:46:57 +0100

    feat: add JWT secret generation to run files

--- run.bat
+++ run.bat
@@ -1,3 +1,10 @@
+# start Docker
 docker-compose up -d
+
+# generate JWT_SECRET (64 bytes, Base64)
+$env:JWT_SECRET = [Convert]::ToBase64String((1..64 | ForEach-Object {Get-Random -Maximum 256}))
+
+# start the Spring Boot app with populate-database profile
 java -jar .\asobo-0.0.1-SNAPSHOT.jar --spring.profiles.active=populate-database
+
 pause
