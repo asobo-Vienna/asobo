0d34e0041ae5c2f01b52610b2504a20ad7678b19
mikeonefive <125759639+mikeonefive@users.noreply.github.com>
2026-02-11 16:03:21 +0100

    refactor: get rid of checkstyle violations

--- backend/src/main/java/at/msm/asobo/config/SecurityConfig.java
+++ backend/src/main/java/at/msm/asobo/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package at.msm.asobo.config;
 import at.msm.asobo.security.CustomUserDetailsService;
 import at.msm.asobo.security.RestAuthenticationEntryPoint;
 import at.msm.asobo.security.TokenAuthenticationFilter;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -20,104 +21,110 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
-import java.util.List;
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private final TokenAuthenticationFilter tokenAuthenticationFilter;
-    private final CustomUserDetailsService customUserDetailsService;
-    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+  private final TokenAuthenticationFilter tokenAuthenticationFilter;
+  private final CustomUserDetailsService customUserDetailsService;
+  private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
 
-    public SecurityConfig(
-            TokenAuthenticationFilter tokenAuthenticationFilter,
-            CustomUserDetailsService customUserDetailsService,
-            RestAuthenticationEntryPoint restAuthenticationEntryPoint) {
-        this.tokenAuthenticationFilter = tokenAuthenticationFilter;
-        this.customUserDetailsService = customUserDetailsService;
-        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
-    }
+  public SecurityConfig(
+      TokenAuthenticationFilter tokenAuthenticationFilter,
+      CustomUserDetailsService customUserDetailsService,
+      RestAuthenticationEntryPoint restAuthenticationEntryPoint) {
+    this.tokenAuthenticationFilter = tokenAuthenticationFilter;
+    this.customUserDetailsService = customUserDetailsService;
+    this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
+  }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    http.addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
-        http.cors(
-                cors ->
-                        cors.configurationSource(
-                                request -> {
-                                    CorsConfiguration config = new CorsConfiguration();
-                                    config.setAllowedOrigins(
-                                            List.of(
-                                                    "http://localhost:4200" // frontend
-                                            ));
-                                    config.setAllowedMethods(
-                                            List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-                                    config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
-                                    config.setAllowCredentials(
-                                            false); // if set to true: allow cookies or auth headers
-                                    return config;
-                                }));
+    http.cors(
+        cors ->
+            cors.configurationSource(
+                request -> {
+                  CorsConfiguration config = new CorsConfiguration();
+                  config.setAllowedOrigins(
+                      List.of(
+                          "http://localhost:4200" // frontend
+                          ));
+                  config.setAllowedMethods(
+                      List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+                  config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
+                  config.setAllowCredentials(
+                      false); // if set to true: allow cookies or auth headers
+                  return config;
+                }));
 
-        // CSRF (cross-site request forgery) protection is not needed, because application uses
-        // stateless
-        // JWT-based authentication and not cookie-based sessions.
-        http.csrf(AbstractHttpConfigurer::disable);
+    // CSRF (cross-site request forgery) protection is not needed, because application uses
+    // stateless
+    // JWT-based authentication and not cookie-based sessions.
+    http.csrf(AbstractHttpConfigurer::disable);
 
-        http.sessionManagement(conf -> conf.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    http.sessionManagement(conf -> conf.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        // disables Spring Security's default form-based login.
-        http.formLogin(AbstractHttpConfigurer::disable);
+    // disables Spring Security's default form-based login.
+    http.formLogin(AbstractHttpConfigurer::disable);
 
-        http.authorizeHttpRequests(
-                registry ->
-                        registry
-                                .requestMatchers(
-                                        "/",
-                                        "/index.html",
-                                        "/assets/**",
-                                        "/images/**",
-                                        "/media/**",
-                                        "**.js",
-                                        "**.css",
-                                        "**.png",
-                                        "**.jpg",
-                                        "**.svg",
-                                        "**.ico")
-                                .permitAll()
-                                .requestMatchers("/api/roles/**").hasAnyRole("ADMIN", "SUPERADMIN")
-                                .requestMatchers("/api/auth/**").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/api/events/**").permitAll()
-                                .requestMatchers("/api/users/**").hasAnyRole("USER", "ADMIN", "SUPERADMIN")
-                                .requestMatchers("/api/search").permitAll()
-                                .requestMatchers("/api/admin/**").hasAnyRole("ADMIN", "SUPERADMIN")
-                                .requestMatchers("/uploads/**").permitAll()
-                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                                .anyRequest()
-                                .authenticated());
+    http.authorizeHttpRequests(
+        registry ->
+            registry
+                .requestMatchers(
+                    "/",
+                    "/index.html",
+                    "/assets/**",
+                    "/images/**",
+                    "/media/**",
+                    "**.js",
+                    "**.css",
+                    "**.png",
+                    "**.jpg",
+                    "**.svg",
+                    "**.ico")
+                .permitAll()
+                .requestMatchers("/api/roles/**")
+                .hasAnyRole("ADMIN", "SUPERADMIN")
+                .requestMatchers("/api/auth/**")
+                .permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/events/**")
+                .permitAll()
+                .requestMatchers("/api/users/**")
+                .hasAnyRole("USER", "ADMIN", "SUPERADMIN")
+                .requestMatchers("/api/search")
+                .permitAll()
+                .requestMatchers("/api/admin/**")
+                .hasAnyRole("ADMIN", "SUPERADMIN")
+                .requestMatchers("/uploads/**")
+                .permitAll()
+                .requestMatchers(HttpMethod.OPTIONS, "/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated());
 
-        // throws HTTP Error 401 with message 'Invalid credentials or authentication required' if
-        // token is expired/invalid/missing
-        http.exceptionHandling(
-                exception -> exception.authenticationEntryPoint(restAuthenticationEntryPoint));
+    // throws HTTP Error 401 with message 'Invalid credentials or authentication required' if
+    // token is expired/invalid/missing
+    http.exceptionHandling(
+        exception -> exception.authenticationEntryPoint(restAuthenticationEntryPoint));
 
-        return http.build();
-    }
+    return http.build();
+  }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-    @Bean
-    public AuthenticationManager authenticationManager(PasswordEncoder passwordEncoder) {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder);
-        provider.setUserDetailsService(customUserDetailsService);
+  @Bean
+  public AuthenticationManager authenticationManager(PasswordEncoder passwordEncoder) {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setPasswordEncoder(passwordEncoder);
+    provider.setUserDetailsService(customUserDetailsService);
 
-        return new ProviderManager(provider);
-    }
+    return new ProviderManager(provider);
+  }
 }
