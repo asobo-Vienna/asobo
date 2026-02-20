9d50bd0370ab3856d943af446a34957ca68819e2
mikeonefive <125759639+mikeonefive@users.noreply.github.com>
2026-02-20 13:55:17 +0100

    refactor: remove logged-in user from method signature

--- backend/src/main/java/at/msm/asobo/controllers/EventAdminController.java
+++ backend/src/main/java/at/msm/asobo/controllers/EventAdminController.java
@@ -4,48 +4,49 @@ import at.msm.asobo.dto.user.UserBasicDTO;
 import at.msm.asobo.dto.user.UserPublicDTO;
 import at.msm.asobo.security.UserPrincipal;
 import at.msm.asobo.services.events.EventAdminService;
-import java.util.Set;
-import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/events/{eventId}/admins")
 @PreAuthorize("hasAnyRole('ADMIN', 'SUPERADMIN', 'USER')")
 public class EventAdminController {
 
-  private final EventAdminService eventAdminService;
+    private final EventAdminService eventAdminService;
 
-  public EventAdminController(EventAdminService eventAdminService) {
-    this.eventAdminService = eventAdminService;
-  }
+    public EventAdminController(EventAdminService eventAdminService) {
+        this.eventAdminService = eventAdminService;
+    }
 
-  @GetMapping()
-  @PreAuthorize("hasAnyRole('USER','ADMIN','SUPERADMIN')")
-  @ResponseStatus(HttpStatus.CREATED)
-  public Set<UserPublicDTO> getEventAdmins(
-      @PathVariable UUID eventId, @AuthenticationPrincipal UserPrincipal loggedInUser) {
-    return eventAdminService.getAllEventAdminsByEventId(eventId);
-  }
+    @GetMapping()
+    @PreAuthorize("hasAnyRole('USER','ADMIN','SUPERADMIN')")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Set<UserPublicDTO> getEventAdmins(
+            @PathVariable UUID eventId) {
+        return eventAdminService.getAllEventAdminsByEventId(eventId);
+    }
 
-  @PatchMapping()
-  @PreAuthorize("hasAnyRole('USER','ADMIN','SUPERADMIN')")
-  @ResponseStatus(HttpStatus.CREATED)
-  public Set<UserBasicDTO> addEventAdmins(
-      @PathVariable UUID eventId,
-      @RequestBody Set<UUID> userIds,
-      @AuthenticationPrincipal UserPrincipal loggedInUser) {
-    return eventAdminService.addAdminsToEvent(eventId, userIds, loggedInUser);
-  }
+    @PatchMapping()
+    @PreAuthorize("hasAnyRole('USER','ADMIN','SUPERADMIN')")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Set<UserBasicDTO> addEventAdmins(
+            @PathVariable UUID eventId,
+            @RequestBody Set<UUID> userIds,
+            @AuthenticationPrincipal UserPrincipal loggedInUser) {
+        return eventAdminService.addAdminsToEvent(eventId, userIds, loggedInUser);
+    }
 
-  @DeleteMapping()
-  @PreAuthorize("hasAnyRole('USER','ADMIN','SUPERADMIN')")
-  public Set<UserBasicDTO> removeEventAdmins(
-      @PathVariable UUID eventId,
-      @RequestBody Set<UUID> userIds,
-      @AuthenticationPrincipal UserPrincipal loggedInUser) {
-    return eventAdminService.removeAdminsFromEvent(eventId, userIds, loggedInUser);
-  }
+    @DeleteMapping()
+    @PreAuthorize("hasAnyRole('USER','ADMIN','SUPERADMIN')")
+    public Set<UserBasicDTO> removeEventAdmins(
+            @PathVariable UUID eventId,
+            @RequestBody Set<UUID> userIds,
+            @AuthenticationPrincipal UserPrincipal loggedInUser) {
+        return eventAdminService.removeAdminsFromEvent(eventId, userIds, loggedInUser);
+    }
 }
