ae8cb5c02f2189da7c04ee868ae7957400fb2cc2
seimes <simon.wolfsteiner@a1.net>
2026-03-02 22:40:29 +0100

    feat: add toast messages on create event page + fix button width on create event page

--- frontend-angular/src/app/features/events/create-event/create-event-form/create-event-form.ts
+++ frontend-angular/src/app/features/events/create-event/create-event-form/create-event-form.ts
@@ -9,6 +9,7 @@ import {environment} from '../../../../../environments/environment';
 import {Textarea} from 'primeng/textarea';
 import {Checkbox} from 'primeng/checkbox';
 import {DateUtils} from '../../../../shared/utils/date/date-utils';
+import {ToastService} from '../../../../shared/services/toast-service';
 
 @Component({
   selector: 'app-create-event-form',
@@ -28,6 +29,7 @@ export class CreateEventForm {
   private formBuilder = inject(FormBuilder);
   private eventService = inject(EventService);
   private authService = inject(AuthService);
+  private toastService = inject(ToastService);
   previewUrl = signal<string | null>(null);
   selectedImage: File | null = null;
 
@@ -46,7 +48,9 @@ export class CreateEventForm {
 
   onSubmit() {
     if (!this.createEventForm.valid) {
-      console.log('Form is invalid, stopping event submission');
+      const errMsg: string = 'Form is invalid, stopping event submission';
+      console.log(errMsg);
+      this.toastService.error(errMsg);
       return;
     }
 
@@ -62,7 +66,9 @@ export class CreateEventForm {
 
     this.eventService.createNewEvent(eventData).subscribe({
       next: (event) => {
-        console.log(`Event ${event.title} created!`);
+        const successMsg: string = `Event ${event.title} created successfully!`;
+        console.log(successMsg);
+        this.toastService.success(successMsg);
 
         if (this.selectedImage) {
           this.uploadEventPicture(event.id);
@@ -71,7 +77,9 @@ export class CreateEventForm {
         }
       },
       error: (err) => {
-        console.log('Error creating new event', err);
+        const errMsg: string = 'Error creating new event';
+        console.log(errMsg, err);
+        this.toastService.error(errMsg);
       }
     });
   }
@@ -86,7 +94,9 @@ export class CreateEventForm {
         this.router.navigate(['/events', eventId]);
       },
       error: (err) => {
-        console.log('Error uploading event picture', err);
+        const errMsg: string = 'Error uploading event picture';
+        console.log(errMsg, err);
+        this.toastService.error(errMsg);
       }
     });
   }
